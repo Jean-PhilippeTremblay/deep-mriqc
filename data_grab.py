009907cc8aaa10c5e989f49710ce41f28607ae0c
Sulantha <sulantha.s@gmail.com>
2017-08-24 15:35:07 -0400

    Adding grabbers.

--- data_grab.py
+++ data_grab.py
@@ -11,3 +11,23 @@ def get_slices_normalised(slices, nif):
 
 def resample_img(moving, fixed):
     return image.resample_to_img(moving, fixed)
+
+
+def get_data(sub_id):
+    base_dir = '/dbh_data/deep_abide'
+    fixed = '50002'
+    file_path = '{base}/{file_base}.nii.gz'.format(base=base_dir, file_base=sub_id)
+    sub_img = resample_img(file_path, '{base}/{file_base}.nii.gz'.format(base=base_dir, file_base=fixed))
+    return get_slices_normalised(40, sub_img)
+
+def get_all_data(sub_list):
+    return list(map(get_data, sub_list))
+
+
+def all_data():
+    ff = open('rater_2_IDs.tsv').readlines()
+    return get_all_data(ff)
+
+
+if __name__ == '__main__':
+    all_data()
