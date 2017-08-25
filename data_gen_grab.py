3be2d181edc508ef9260cb55dc4e71f90ff9941a
Sulantha <sulantha.s@gmail.com>
2017-08-25 11:39:33 -0400

    Adding grabbers.

---
+++ data_gen_grab.py
@@ -0,0 +1,21 @@
+
+
+TRAIN_LIST = []
+TEST_LIST = []
+
+READ_INX = 0
+
+def get_label_from_id(id):
+    ##TODO
+    ### GET LABEL FOR ID IN HERE
+    return None
+
+
+def data_gen(batch_size):
+    ##TODO
+    ### READ BATCHES FROM TRAIN LIST STARTING FROM READ_IDX
+    ### MODIFY READ_IDX AFTERWARDS.
+
+    ### SHOULD RETURN A TUPLE (inputs, targets)
+
+    return None
